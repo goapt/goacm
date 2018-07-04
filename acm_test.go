0695ed0d64088c615ec43403885c68a906b680d2
fifsky <fifsky@gmail.com>
2018-07-04 21:55:07 +0800

    Test with environment variables

--- acm_test.go
+++ acm_test.go
@@ -1,49 +1,56 @@
 package goacm
 
-//func getClient() *Client {
-//	client, err := NewClient(func(c *Client) {
-//		c.AccessKey = "************"
-//		c.SecretKey = "************"
-//		c.EndPoint = "acm.aliyun.com"
-//		c.NameSpace = "************"
-//	})
-//
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	return client
-//}
-//
-//func TestNewClient(t *testing.T) {
-//	client := getClient()
-//	servers := client.GetServers()
-//
-//	if len(servers) == 0 {
-//		t.Error("get server error")
-//	}
-//
-//	fmt.Println(servers)
-//}
-//
-//func TestClient_GetConfig(t *testing.T) {
-//	client := getClient()
-//	ret,err :=client.GetConfig("php-test","payment")
-//
-//	if err != nil {
-//		t.Error(err)
-//	}
-//
-//	fmt.Println(ret)
-//}
-//
-//func TestClient_Subscribe(t *testing.T) {
-//	client := getClient()
-//	ret,err :=client.Subscribe("php-test","payment","")
-//
-//	if err != nil {
-//		t.Error(err)
-//	}
-//
-//	fmt.Println(ret)
-//}
+import (
+	"testing"
+	"fmt"
+	"log"
+	"os"
+)
+
+func getClient() *Client {
+	client, err := NewClient(func(c *Client) {
+		c.AccessKey = os.Getenv("AccessKey")
+		c.SecretKey = os.Getenv("SecretKey")
+		c.EndPoint = "acm.aliyun.com"
+		c.NameSpace = os.Getenv("NameSpace")
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
+
+func TestNewClient(t *testing.T) {
+	client := getClient()
+	servers := client.GetServers()
+
+	if len(servers) == 0 {
+		t.Error("get server error")
+	}
+
+	fmt.Println(servers)
+}
+
+func TestClient_GetConfig(t *testing.T) {
+	client := getClient()
+	ret, err := client.GetConfig("php-test", "payment")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(ret)
+}
+
+func TestClient_Subscribe(t *testing.T) {
+	client := getClient()
+	ret, err := client.Subscribe("php-test", "payment", "")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(ret)
+}
